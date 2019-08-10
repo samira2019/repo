6d7dbd909d0ccb29402f042964b4932f4e36736e
samira2019 <47431650+samira2019@users.noreply.github.com>
2019-08-10 14:46:35 +0430

    my commit

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
-listOne =[[2019, "aug", 6, ['shopping', 'swimming']],
+listOne = [[2019, "aug", 6, ['shopping', 'swimming']],
            [[2019, 'aug', 7, ['dancing', 'play music']]]]
-listOne =listOne * 2
+listOne = listOne * 2
 print(listOne)
-print(6 in listOne[0])
+print(6 in listOne[1])
