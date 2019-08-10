daccd86ef8c28003d1dc1e04507578e412bce05d
samira assarzadeh <samiraassarzadeh2014@gmail.com>
2019-08-10 14:56:47 +0430

    remove

--- list.py
+++ list.py
@@ -2,4 +2,4 @@ listOne = [[2019, "aug", 6, ['shopping', 'swimming']],
            [[2019, 'aug', 7, ['dancing', 'play music']]]]
 listOne = listOne * 2
 print(listOne)
-print(6 in listOne[1])
+print(6 in listOne)
